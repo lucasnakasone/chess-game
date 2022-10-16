783308d41100b569d09108f7feb7f709df57cd7c
Lucas Nakasone <lucasnakasone@hotmail.com>
2022-10-16 20:20:04 -0300

    Implemented - Pawn moves

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -2,6 +2,7 @@ package chess.pieces;
 
 import board.Board;
 import board.BoardException;
+import board.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,8 +18,58 @@ public class Pawn extends ChessPiece{
 	}
 
 	@Override
-	public boolean[][] possibleMoves() {
+	public boolean[][] possibleMoves() throws BoardException {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position p = new Position(0, 0);
+		if(getColor() == Color.WHITE) {
+			// Check 1 above
+			p.setValues(position.getRow() - 1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			// Check 2 above
+			p.setValues(position.getRow() - 2, position.getColumn());
+			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) 
+					&& getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p) 
+					&& getMoveCount() == 0) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			// Check northwest
+			p.setValues(position.getRow() - 1, position.getColumn() - 1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			// Check northeast
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+		} else if (getColor() == Color.BLACK){
+			// Check 1 below
+			p.setValues(position.getRow() + 1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			// Check 2 below
+			p.setValues(position.getRow() + 2, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) 
+					&& getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p) 
+					&& getMoveCount() == 0) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			// Check southwest
+			p.setValues(position.getRow() + 1, position.getColumn() - 1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			// Check southeast
+			p.setValues(position.getRow() + 1, position.getColumn() + 1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+		}
 		return mat;
 	}
 
